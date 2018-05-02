10c4d0782b9adf8f4c4253ad11a36f4ccfe45cf2
Rubén Martín Lozano <ruben.martin.lozano@gmail.com>
2018-05-02 19:43:47 +0200

    update sounds

--- demo/index.js
+++ demo/index.js
@@ -6,13 +6,18 @@ const demo = (
   <Fragment>
     <div className='demo-Custom'>
       <Bpm bpm={60} metronome />
+      <Bpm bpm={120} metronome />
+      <Bpm bpm={240} metronome />
+    </div>
+    <div className='demo-Inverse'>
+      <Bpm bpm={60} />
       <Bpm bpm={120} />
       <Bpm bpm={240} />
     </div>
-    <div className='demo-Inverse'>
-      <Bpm bpm={60} metronome active sound />
-      <Bpm bpm={120} metronome active />
-      <Bpm bpm={240} metronome active />
+    <div className='demo-Custom'>
+      <Bpm bpm={60} metronome sound />
+      <Bpm bpm={120} metronome sound />
+      <Bpm bpm={240} metronome sound />
     </div>
     <Bpm bpm={60} metronome sound waltz />
     <Bpm bpm={190} metronome sound waltz />
